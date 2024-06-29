fe894e1577f56c45b85dc93989e2095dd18072e8
tbrandirali <tbrandirali-dev+github@pm.me>
2024-06-29 14:05:32 +0200

    Cleanup after db switch

--- chatminer/app.py
+++ chatminer/app.py
@@ -1,10 +1,8 @@
 import re
 import milc
 
-from chatminer import parser, viz, config, sqlitedb
+from chatminer import parser, viz, db
 from chatminer.common import log, error
-
-configs = config.configs
 
 
 @milc.cli.entrypoint('This is chatminer! Specify a command to use...')
@@ -24,11 +22,11 @@ def create(cli: milc.MILC) -> None:
 
     messages, notifications = parser.parse(lines)
 
-    with sqlitedb.create_connection() as conn:
-        sqlitedb.create_messages_table(conn, chat_name)
-        sqlitedb.insert_messages(conn, chat_name, messages)
-        sqlitedb.create_notifications_table(conn, f'{chat_name}_notifications')
-        sqlitedb.insert_notifications(conn, f'{chat_name}_notifications', notifications)
+    with db.create_connection() as conn:
+        db.create_messages_table(conn, chat_name)
+        db.insert_messages(conn, chat_name, messages)
+        db.create_notifications_table(conn, f'{chat_name}_notifications')
+        db.insert_notifications(conn, f'{chat_name}_notifications', notifications)
 
 
 @milc.cli.argument('-c', '--chat', help="The name of the chat to delete")
@@ -36,9 +34,9 @@ def create(cli: milc.MILC) -> None:
 def delete(cli: milc.MILC) -> None:
     chat_name = cli.args.chat if cli.args.chat else missing_arg("Chat name is required")
 
-    with sqlitedb.create_connection() as conn:
-        sqlitedb.delete_table(conn, chat_name)
-        sqlitedb.delete_table(conn, f"{chat_name}_notifications")
+    with db.create_connection() as conn:
+        db.delete_table(conn, chat_name)
+        db.delete_table(conn, f"{chat_name}_notifications")
 
 
 @milc.cli.argument('-c', '--chat', help="The database table to plot frequency for")
@@ -47,8 +45,8 @@ def delete(cli: milc.MILC) -> None:
 def frequency(cli: milc.MILC) -> None:
     chat_name = cli.args.chat if cli.args.chat else missing_arg("Chat name is required")
 
-    with sqlitedb.create_connection() as conn:
-        messages = sqlitedb.get_all_messages(conn, chat_name)
+    with db.create_connection() as conn:
+        messages = db.get_all_messages(conn, chat_name)
 
     keyword = cli.args.keyword
     if keyword is not None:
@@ -62,8 +60,8 @@ def frequency(cli: milc.MILC) -> None:
 def frequency_per_sender(cli: milc.MILC) -> None:
     chat_name = cli.args.chat if cli.args.chat else missing_arg("Chat name is required")
 
-    with sqlitedb.create_connection() as conn:
-        messages = sqlitedb.get_all_messages(conn, chat_name)
+    with db.create_connection() as conn:
+        messages = db.get_all_messages(conn, chat_name)
 
     keyword = cli.args.keyword
     if keyword is not None:
@@ -71,23 +69,9 @@ def frequency_per_sender(cli: milc.MILC) -> None:
     viz.plot_frequency_per_sender(messages, keyword)
 
 
-@milc.cli.argument('-n', '--name', help="The name of the configuration item to set, in dot notation (ex: 'database.host')")
-@milc.cli.argument('-v', '--value', help="The value to set the configuration item to")
-@milc.cli.argument('-r', '--reset', action="store_true", help="Reset the chatminer configurations to default values")
-@milc.cli.subcommand("Set a chatminer configuration item, using dot notation\nExample: 'chatminer set-config -n database.password -v myPassword'")
-def configure(cli: milc.MILC) -> None:
-    if cli.args.reset:
-        config.reset_configs()
-    else:
-        name = cli.args.name if cli.args.name else missing_arg("Config item name is required")
-        value = cli.args.value if cli.args.value else missing_arg("Config item value is required")
-        config.set_config(name, value)
-
-
 @milc.cli.subcommand("Removes all files and databases persisted by chatminer on the local system")
 def uninstall(cli: milc.MILC) -> None:
-    config.delete_configs()
-    sqlitedb.delete_database()
+    db.delete_database()
 
 
 def missing_arg(arg_name: str) -> None:
